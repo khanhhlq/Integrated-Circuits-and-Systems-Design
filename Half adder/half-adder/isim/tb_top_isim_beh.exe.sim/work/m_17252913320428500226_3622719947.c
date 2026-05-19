0233b37d2d36472190cf03370e2ad9ce0fbaafe5
khanhhlq <lee.quockhanh1107@gmail.com>
2026-05-19 11:04:31 +0700

    done half-adder

--- Half adder/half-adder/isim/tb_top_isim_beh.exe.sim/work/m_17252913320428500226_3622719947.c
+++ Half adder/half-adder/isim/tb_top_isim_beh.exe.sim/work/m_17252913320428500226_3622719947.c
@@ -21,11 +21,44 @@
 #include <malloc.h>
 #define alloca _alloca
 #endif
-static const char *ng0 = "/home/ise/D_DRIVE/Integrated-Circuits-and-Systems-Design/half adder/testbench.v";
-static int ng1[] = {0, 0};
-static int ng2[] = {1, 0};
+static const char *ng0 = "At time %0t: a=%b b=%b, sum=%b, carry = %b";
+static const char *ng1 = "/home/ise/D_DRIVE/Integrated-Circuits-and-Systems-Design/half adder/testbench.v";
+static int ng2[] = {0, 0};
+static int ng3[] = {1, 0};
+
+void Monitor_8_1(char *);
+void Monitor_8_1(char *);
 
 
+static void Monitor_8_1_Func(char *t0)
+{
+    char t1[16];
+    char *t2;
+    char *t3;
+    char *t4;
+    char *t5;
+    char *t6;
+    char *t7;
+    char *t8;
+    char *t9;
+    char *t10;
+    char *t11;
+
+LAB0:    t2 = xsi_vlog_time(t1, 1000.0000000000000, 1000.0000000000000);
+    t3 = (t0 + 1608);
+    t4 = (t3 + 56U);
+    t5 = *((char **)t4);
+    t6 = (t0 + 1768);
+    t7 = (t6 + 56U);
+    t8 = *((char **)t7);
+    t9 = (t0 + 1048U);
+    t10 = *((char **)t9);
+    t9 = (t0 + 1208U);
+    t11 = *((char **)t9);
+    xsi_vlogfile_write(1, 0, 3, ng0, 6, t0, (char)118, t1, 64, (char)118, t5, 1, (char)118, t8, 1, (char)118, t10, 1, (char)118, t11, 1);
+
+LAB1:    return;
+}
 
 static void Initial_7_0(char *t0)
 {
@@ -40,74 +73,80 @@ LAB0:    t1 = (t0 + 2680U);
 
 LAB3:    goto *t2;
 
-LAB2:    xsi_set_current_line(7, ng0);
+LAB2:    xsi_set_current_line(7, ng1);
 
-LAB4:    xsi_set_current_line(9, ng0);
-    t2 = ((char*)((ng1)));
+LAB4:    xsi_set_current_line(8, ng1);
+    Monitor_8_1(t0);
+    xsi_set_current_line(9, ng1);
+    t2 = ((char*)((ng2)));
     t3 = (t0 + 1608);
     xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
-    xsi_set_current_line(9, ng0);
-    t2 = ((char*)((ng1)));
+    xsi_set_current_line(9, ng1);
+    t2 = ((char*)((ng2)));
     t3 = (t0 + 1768);
     xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
-    xsi_set_current_line(10, ng0);
+    xsi_set_current_line(10, ng1);
     t2 = (t0 + 2488);
-    xsi_process_wait(t2, 10000LL);
+    xsi_process_wait(t2, 200000LL);
     *((char **)t1) = &&LAB5;
 
 LAB1:    return;
-LAB5:    xsi_set_current_line(13, ng0);
-    t2 = ((char*)((ng1)));
+LAB5:    xsi_set_current_line(11, ng1);
+    t2 = ((char*)((ng2)));
     t3 = (t0 + 1608);
     xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
-    xsi_set_current_line(13, ng0);
-    t2 = ((char*)((ng2)));
+    xsi_set_current_line(11, ng1);
+    t2 = ((char*)((ng3)));
     t3 = (t0 + 1768);
     xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
-    xsi_set_current_line(14, ng0);
+    xsi_set_current_line(12, ng1);
     t2 = (t0 + 2488);
-    xsi_process_wait(t2, 10000LL);
+    xsi_process_wait(t2, 200000LL);
     *((char **)t1) = &&LAB6;
     goto LAB1;
 
-LAB6:    xsi_set_current_line(17, ng0);
-    t2 = ((char*)((ng2)));
+LAB6:    xsi_set_current_line(13, ng1);
+    t2 = ((char*)((ng3)));
     t3 = (t0 + 1608);
     xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
-    xsi_set_current_line(17, ng0);
-    t2 = ((char*)((ng1)));
+    xsi_set_current_line(13, ng1);
+    t2 = ((char*)((ng2)));
     t3 = (t0 + 1768);
     xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
-    xsi_set_current_line(18, ng0);
+    xsi_set_current_line(14, ng1);
     t2 = (t0 + 2488);
-    xsi_process_wait(t2, 10000LL);
+    xsi_process_wait(t2, 200000LL);
     *((char **)t1) = &&LAB7;
     goto LAB1;
 
-LAB7:    xsi_set_current_line(21, ng0);
-    t2 = ((char*)((ng2)));
+LAB7:    xsi_set_current_line(15, ng1);
+    t2 = ((char*)((ng3)));
     t3 = (t0 + 1608);
     xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
-    xsi_set_current_line(21, ng0);
-    t2 = ((char*)((ng2)));
+    xsi_set_current_line(15, ng1);
+    t2 = ((char*)((ng3)));
     t3 = (t0 + 1768);
     xsi_vlogvar_assign_value(t3, t2, 0, 0, 1);
-    xsi_set_current_line(22, ng0);
-    t2 = (t0 + 2488);
-    xsi_process_wait(t2, 10000LL);
-    *((char **)t1) = &&LAB8;
     goto LAB1;
 
-LAB8:    xsi_set_current_line(24, ng0);
-    xsi_vlog_finish(1);
-    goto LAB1;
+}
 
+void Monitor_8_1(char *t0)
+{
+    char *t1;
+    char *t2;
+
+LAB0:    t1 = (t0 + 2736);
+    t2 = (t0 + 3248);
+    xsi_vlogfile_monitor((void *)Monitor_8_1_Func, t1, t2);
+
+LAB1:    return;
 }
 
 
 extern void work_m_17252913320428500226_3622719947_init()
 {
-	static char *pe[] = {(void *)Initial_7_0};
+	static char *pe[] = {(void *)Initial_7_0,(void *)Monitor_8_1};
 	xsi_register_didat("work_m_17252913320428500226_3622719947", "isim/tb_top_isim_beh.exe.sim/work/m_17252913320428500226_3622719947.didat");
 	xsi_register_executes(pe);
 }
--- Half adder/testbench.v
+++ Half adder/testbench.v
@@ -1,27 +1,17 @@
-`timescale 1ns/1ps
-
 module tb_top;
     reg a, b;
     wire s, c_out;
-    half_adder ha ( .a(a), .b(b), .s(s), .cout(c_out));
+
+    half_adder ha(a, b, s, c_out);
+
     initial begin
-        // Test case 1
+        $monitor("At time %0t: a=%b b=%b, sum=%b, carry = %b", $time, a,b,s,c_out);
         a = 0; b = 0;
-        #10;
-
-        // Test case 2
+        #200;
         a = 0; b = 1;
-        #10;
-
-        // Test case 3
+        #200;
         a = 1; b = 0;
-        #10;
-
-        // Test case 4
+        #200;
         a = 1; b = 1;
-        #10;
-
-        $finish;
     end
-
 endmodule
